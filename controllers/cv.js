d19194cabc6ba1039e692e11cd2fa20e76566c83
geryakbar <geryakbarfauzi@gmail.com>
2020-08-21 10:53:12 +0700

    - CV Done - Project Done - Home Done - SQL Database Updated
    Known Bug :
    - None (You told me)

--- controllers/cv.js
+++ controllers/cv.js
@@ -2,6 +2,7 @@
 const Identity = require('../models/identitas');
 const Skill = require('../models/kemampuan');
 const History = require('../models/riwayat');
+const Introduction = require('../models/introduction');
 //method untuk menampilkan halaman cv
 exports.showCV = (req, res, next) => {
     Identity.findAll()
@@ -17,13 +18,22 @@ exports.showCV = (req, res, next) => {
                             tipe: "Sekolah"
                         }
                     }).then(historySchool => {
-                        res.render('cv', {
-                            pageTitle: 'CV Saya',
-                            identity: identity,
-                            kemampuan: skill,
-                            sekolah: historySchool,
-                            perusahaan: historyWork
-                        });
+                        Introduction.findAll({
+                            where: {
+                                id: 1
+                            }
+                        }).then(intro => {
+                            res.render('cv', {
+                                pageTitle: 'CV Saya',
+                                identity: identity,
+                                kemampuan: skill,
+                                sekolah: historySchool,
+                                perusahaan: historyWork,
+                                introduction: intro
+                            });
+                        }).catch(err => {
+                            console.log(err)
+                        })
                     }).catch(err => {
                         console.log(err)
                     })
